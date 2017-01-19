c84497e9cb999345c5004fb6ad904603381805d2
marcusah <makasuh@gmail.com>
2017-01-19 16:51:48 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -59,45 +59,8 @@ class CoffeeScreen(Screen):
 	h2o_ftemp = StringProperty('')
 	h2o_ctemp = StringProperty('')
 
-	def __init__(self, **kwargs):
-			
+	def __init__(self, **kwargs):			
 		super(CoffeeScreen, self).__init__(**kwargs)
-		Clock.schedule_once(self._finish_init)
-		
-
-	def _finish_init(self,dt):
-		self.water_lights=self.ids.water_lv_lights
-		self.alt_light= self.ids.altitude_light
-		self.air_light=self.ids.air_pump_light
-		self.water_pump=self.ids.water_pump_light
-		self.water_vol=self.ids.water_vol
-		self.door_light=self.ids.pod_door_light
-		self.color = self.ids.pod_color
-		self.carafe_light= self.ids.carafe_light
-		self.led_light= self.ids.led_light
-		self.water_value=self.manager.get_screen('control').ids.water_lv_knob.value 
-		self.alt_on=self.manager.get_screen('control').ids.alt_mode_on
-		self.alt_off=self.manager.get_screen('control').ids.alt_mode_off
-		self.air_on=self.manager.get_screen('control').ids.air_pump_on
-		self.air_off=self.manager.get_screen('control').ids.air_pump_off
-		self.water_on=self.manager.get_screen('control').ids.water_pump_on
-		self.water_off=self.manager.get_screen('control').ids.water_pump_off
-		self.sizes=self.manager.get_screen('control').ids.sizes
-		self.door_open=self.manager.get_screen('control').ids.pod_door_open
-		self.door_closed=self.manager.get_screen('control').ids.pod_door_closed
-		self.pod_present=self.manager.get_screen('control').ids.pod_present
-		self.pos_missing=self.manager.get_screen('control').ids.pod_missing
-		self.pod_options=self.manager.get_screen('control').ids.pod_options
-		self.carafe_present=self.manager.get_screen('control').ids.carafe_present
-		self.carafe_missing=self.manager.get_screen('control').ids.carafe_missing
-		self.led_off=self.manager.get_screen('control').ids.led_off
-		self.led_on=self.manager.get_screen('control').ids.led_on
-		self.led_flash=self.manager.get_screen('control').ids.led_flash
-#		self.set_temp=self.manager.get_screen('control').set_temp
-		self.temp_value=self.manager.get_screen('control').temp_value
-		self.my_temp=self.manager.get_screen('control').ids.my_temp
-
-
 
 class ControlScreen(Screen):
 	global event
@@ -108,11 +71,14 @@ class ControlScreen(Screen):
 	min=NumericProperty(80)
 	max=NumericProperty(212)
 	temp_value=NumericProperty(150)
+	size_value=NumericProperty(6)
+	min_size =NumericProperty(4)
+	max_size =NumericProperty(14)
 	def __init__(self, **kwargs):
 		
 		super(ControlScreen, self).__init__(**kwargs)
 		Clock.schedule_once(self._finish_init,0.5)
-		Clock.schedule_interval(self.disp_size,0.5)
+		Clock.schedule_interval(self.disp_size,0.1)
 		Clock.schedule_interval(self.disp_pod,0.5)
 		Clock.schedule_interval(self.disp_temp,0.1)
 		Clock.schedule_interval(self.water_level,0.5)
@@ -121,88 +87,107 @@ class ControlScreen(Screen):
 		self.alt_light= self.manager.get_screen('coffee').ids.altitude_light
 		self.air_light= self.manager.get_screen('coffee').ids.air_pump_light
 		self.water_light=self.manager.get_screen('coffee').ids.water_pump_light
-		self.water_vol=self.manager.get_screen('coffee').ids.water_vol
 		self.door_light=self.manager.get_screen('coffee').ids.pod_door_light
 		self.color = self.manager.get_screen('coffee').ids.pod_color
 		self.carafe_light= self.manager.get_screen('coffee').ids.carafe_light
 		self.led_light= self.manager.get_screen('coffee').ids.led_light
 		self.pod_light= self.manager.get_screen('coffee').ids.pod_light
 		self.home_sensor = self.manager.get_screen('coffee').ids.home_sensor
-		self.water_value=self.ids.water_lv_knob.value 
-		self.alt_on=self.ids.alt_mode_on
-		self.alt_off=self.ids.alt_mode_off
-		self.air_on=self.ids.air_pump_on
-		self.air_off=self.ids.air_pump_off
-		self.water_on=self.ids.water_pump_on
-		self.water_off=self.ids.water_pump_off
-		self.sizes=self.ids.sizes
-		self.door_open=self.ids.pod_door_open
-		self.door_closed=self.ids.pod_door_closed
-		self.pod_present=self.ids.pod_present
-		self.pos_missing=self.ids.pod_missing
 		self.pod_options=self.ids.pod_options
-		self.carafe_present=self.ids.carafe_present
-		self.carafe_missing=self.ids.carafe_missing
-		self.led_off=self.ids.led_off
-		self.led_on=self.ids.led_on
-		self.led_flash=self.ids.led_flash
-		self.my_temp= self.ids.my_temp
 		self.f_to_c()
+		self.alt_status=False
+		self.air_status=False
+		self.water_status=False
+		self.door_status=False
+		self.pod_status=False
+		self.carafe_status=False
+		self.home_status=False
+
+	def alt_mode_on_off(self):
+		if self.alt_status == False:
+			self.alt_light.turn_on_l1()
+			self.alt_status = True
+		else:
+			self.alt_light.turn_off_l1()
+			self.alt_status = False
+
+	def turn_air_on_off(self):
+		if self.air_status == False:
+			self.air_light.turn_on_l1()
+			self.air_status = True
+		else:
+			self.air_light.turn_off_l1()
+			self.air_status = False
+
+	def turn_water_on_off(self):
+		if self.water_status == False:
+			self.water_light.turn_on_l1()
+			self.water_status = True
+		else:
+			self.water_light.turn_off_l1()
+			self.water_status = False
 
 
-	def alt_mode_on(self):
-		self.alt_light.turn_on_l1()
-	def alt_mode_off(self):
-		self.alt_light.turn_off_l1()
-	def turn_air_on(self):
-		self.air_light.turn_on_l1()
-	def turn_air_off(self):
-		self.air_light.turn_off_l1()
-	def turn_water_on(self):
-		self.water_light.turn_on_l1()
-	def turn_water_off(self):
-		self.water_light.turn_off_l1()
-	def open_door(self):
-		self.door_light.color1='red'
-	def close_door(self):
-		self.door_light.color1='green'
-	def insert_pod(self):
-		self.pod_light.color1='green'
-	def remove_pod(self):
-		self.pod_light.color1='red'
-	def insert_carafe(self):
-		self.carafe_light.color1='green'
-	def remove_carafe(self):
-		self.carafe_light.color1='red'
+	def open_close_door(self):
+		if self.door_status == False:
+			self.door_light.color1='green'
+			self.door_status =True
+		else:
+			self.door_light.color1='red'
+			self.door_status =False
 
-	def home_on(self):
-		self.home_sensor.turn_on_l1()
-	def home_off(self):
-		self.home_sensor.turn_off_l1()
+	def insert_remove_pod(self):
+		if self.pod_status==False:
+			self.pod_light.color1='green'
+			self.pod_status=True
+		else:
+			self.pod_light.color1='red'
+			self.pod_status = False
+		
+	def insert_remove_carafe(self):
+		if self.carafe_status == False:
+			self.carafe_light.color1='green'
+			self.carafe_status = True
+		else:
+			self.carafe_light.color1='red'
+			self.carafe_status = False
+	def home_on_off(self):
+		if self.home_status == False:
+			self.home_sensor.turn_on_l1()
+			self.home_status = True
+		else:
+			self.home_sensor.turn_off_l1()
+			self.home_status = False
+
 	def LED_on(self):
 		try:
 			self.event.cancel()
 		except:
 			pass
 		self.led_light.turn_on_l1()
+
 	def LED_off(self):
 		try:
 			self.event.cancel()
 		except:
-			pass
-		
+			pass		
 		self.led_light.turn_off_l1()
+
 	def LED_flash(self):
 		self.event=Clock.schedule_interval(self.flash,0.1)
+
 	def flash(self,dt):
 		if self.led_light.bol1==True:
 			self.led_light.bol1 =False
 		else:
 			self.led_light.bol1=True
+
 	def disp_size(self,dt):
-		self.size_text=self.sizes.text
+		self.size_text=str(self.size_value)
+
 	def disp_pod(self,dt):
 		self.pod_text = self.pod_options.text
+
 	def water_level(self,dt):
 		self.water_lights=self.manager.get_screen('coffee').ids.water_lv_lights
 		self.water_value=self.ids.water_lv_knob.value 
@@ -225,27 +210,29 @@ class ControlScreen(Screen):
 		self.set_ftemp = str(self.temp_value)
 
 	def f_to_c(self):
-
 		self.current_ctemp= str(round((self.temp_value -32)/1.8,1))
 		self.set_ctemp = self.current_ctemp
 		
-		
-		
 	def increase_temp(self):
-
-
 		if self.temp_value<self.max:
 			self.temp_value+=1
 			self.f_to_c()
 			
-
-
 	def decrease_temp(self):
-		
 		if self.temp_value>self.min:
 			self.temp_value-=1
 			self.f_to_c()
-#Building the app. The program will look for the file "nuclear.kv" because the app is called Nuclear			
+
+	def increase_size(self):
+		if self.size_value<self.max_size:
+			self.size_value+=0.5
+
+	def decrease_size(self):
+		
+		if self.size_value>self.min_size:
+			self.size_value-=0.5
+			
+#Building the app. The program will look for the file "coffee.kv" because the app is called Coffee			
 class CoffeeApp(App):
 	def build(self):
 		Config.set('graphics','fullscreen', True)
